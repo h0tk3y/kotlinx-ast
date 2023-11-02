05b67c2f1c92d4e979d7d30bd72a23c8f166e6f7
Sergey Igushkin <hotkeytlt@gmail.com>
2023-11-02 13:19:19 +0400

    Target Java 8 in the JVM part of ast-common

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -4,7 +4,13 @@ plugins {
 }
 
 kotlin {
-    jvm()
+    jvm {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
 
     sourceSets {
         val commonMain by getting {
